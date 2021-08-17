319feeb0bdd684b24604e7ec47654336edd7958e
Phillip Chan <pchansoft@gmail.com>
2021-08-17 11:41:21 -0400

    Adding files via VSC

---
+++ json-examples.js
@@ -0,0 +1,10 @@
+const myObject = {a: "this is a property a",
+mySecondObjectProperty: "second object property",
+"x": "the x key on my object",
+"2": "the number 2",
+3: "key that is 3"
+}
+
+console.log("my object", myObject);
+
+const validJSONObject
